4bd78fdce3186033ed95b0dd69c2ca79b895ee84
Andy Awidarto <andy@sinaptix.com>
2011-12-20 23:26:01 +0700

    new api with sample script

--- application/controllers/gantibaju.php
+++ application/controllers/gantibaju.php
@@ -20,7 +20,7 @@ class Gantibaju extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
-		$this->api_key = 'c07c4f4b23b770c2d4e8d6d108b5c304472df238';
+		$this->api_key = '2bf7ec5f0f3f1b5664f34e58bc7ea33cf6eb9860';
 	}
 	
 	public function index($appkey = null,$trxid = null){
@@ -62,7 +62,75 @@ class Gantibaju extends CI_Controller {
 		print $result;
 	}
 	
-	public function checkout(){
+	public function ajaxpost($api_key){
+		$this->load->helper('string');
+		$this->load->library('curl');
+		
+		/*
+			for this example only :
+			trx_id is a string of 16 digit randomly generated numbers
+			transactions table is a table containing generated trx_id only, transaction detail is fixed and hardcoded below
+		$trx_id = random_string('numeric',16);
+		*/
+		
+		$trx_id = $this->input->post('trx_id');
+		$buyer_name = $this->input->post('buyer_name');
+		$recipient_name = $this->input->post('recipient_name');
+		$shipping_address = $this->input->post('shipping_address');
+		$email = $this->input->post('email');
+		$buyerdeliveryzone = $this->input->post('buyerdeliveryzone');
+		$buyerdeliverytime = $this->input->post('buyerdeliverytime');
+		$phone = $this->input->post('phone');
+		$status = $this->input->post('status');
+
+		//$this->db->insert('transactions',array('trx_id'=>$trx_id));
+
+		$url = 'http://localhost/jayonadmin/api/v1/post/'.$api_key.'/'.$trx_id;
+		
+		$trx = array(
+			'api_key'=>$api_key,
+			'transaction_id'=>$trx_id,
+			'buyer_name'=>$buyer_name,
+			'recipient_name'=>$recipient_name,
+			'shipping_address'=>$shipping_address,
+			'buyerdeliveryzone'=>$buyerdeliveryzone,
+			'buyerdeliverytime'=>$buyerdeliverytime,
+			'email'=>$email,
+			'phone' => $phone,
+			'cod_cost' => '0', 		// cod_cost 0 if absorbed in price of goods sold, otherwise specify the amount here
+			'currency' => 'IDR', 	// currency in 3 digit codes
+			'status'=>$status, 	// status can be : incoming or confirm, depending on merchant's workflow
+			'trx_detail'=>array(
+				array(
+					'unit_description'=>'kaos oblong swan',
+					'unit_price'=>3000,
+					'unit_quantity'=>100,
+					'unit_total'=>280000,
+					'unit_discount'=>20000
+				),
+				array(
+					'unit_description'=>'kaos turtle neck',
+					'unit_price'=>35000,
+					'unit_quantity'=>2,
+					'unit_total'=>70000,
+					'unit_discount'=>0,
+				),
+				array(
+					'unit_description'=>'kaos kutung',
+					'unit_price'=>15000,
+					'unit_quantity'=>10,
+					'unit_total'=>150000,
+					'unit_discount'=>0
+				)
+			)
+		);
+		
+		$result = $this->curl->simple_post($url,array('transaction_detail'=>json_encode($trx)));
+		
+		print $result;
+	}	
+	
+	public function plaincheckout(){
 		$this->load->helper('string');
 
 		$trx_id = random_string('numeric',16);
@@ -74,9 +142,23 @@ class Gantibaju extends CI_Controller {
 		$this->load->view('shoppingtest',$data);
 		
 	}
-	
-	public function jayon(){
+
+	public function checkout(){
+		$this->load->helper('string');
 		
+		$this->load->config('zones');
+		$this->load->helper('form');
+		
+
+		$trx_id = random_string('numeric',16);
+		
+		$this->db->insert('transactions',array('trx_id'=>$trx_id));
+		
+		$data['trx_id'] = $trx_id;
+		
+		$data['api_key'] = $this->api_key;
+
+		$this->load->view('process',$data);
 	}
 	
 	public function jayonfetch($api_key,$trx_id){
---
+++ application/views/process.php
@@ -0,0 +1,139 @@
+<?php
+	//print_r($trx_detail);
+?>
+<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/css/style.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="<?php print base_url();?>assets/css/jquery-ui/flick/jquery-ui-1.8.16.custom.css" media="screen" />
+
+<script type="text/javascript" src="<?php print base_url();?>assets/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="<?php print base_url();?>assets/js/jquery-ui/jquery-ui-1.8.16.custom.min.js"></script>
+<script type="text/javascript" src="<?php print base_url();?>assets/js/jquery-ui/jquery-ui-timepicker-addon.js"></script>
+
+<script>
+	$(document).ready(function() {
+		$( '#buyerdeliverytime' ).datetimepicker({
+			dateFormat:'yy-mm-dd',
+			showSecond: false,
+			timeFormat: 'hh:mm:ss',
+			stepHour: 2,
+			stepMinute: 30
+		});
+		
+		$( '#buyerdeliveryzone' ).autocomplete({
+			source: '<?php print site_url('buy/getzone')?>',
+			method: 'post',
+			minLength: 2
+		});
+
+	} );
+	
+	function toggleForm(){
+		$('#orderform').toggle();
+	}
+	
+	function ajaxpost(status){
+
+		var data = {
+			trx_id:$('#trx_id').val(),
+			buyerdeliveryzone:$('#buyerdeliveryzone option:selected').val(),
+			buyerdeliverytime:$('#buyerdeliverytime').val(),
+			shipping_address:$('#shipping_address').val(),
+			buyer_name:$('#buyer_name').val(),
+			recipient_name:$('#recipient_name').val(),
+			phone:$('#phone').val(),
+			email:$('#email').val(),
+			status:status
+		};
+		
+		$.ajax({
+		  type: 'POST',
+		  url: '<?php print site_url('gantibaju/ajaxpost/'.$api_key);?>',
+		  data: data,
+		  success: function(data){
+				alert(data);
+			},
+		  dataType: 'json'
+		});
+	}
+	
+</script>
+
+<p>
+	<span style="cursor:pointer;padding:8px;border:thin solid maroon;margin-top:35px;display:block;width:85px;" onClick="javascript:toggleForm();">COD By Jayon</span>
+</p>
+
+<div id="orderform" style="display:none;">
+<?php print form_open();?>
+
+<h2>Delivery Order</h2>
+
+<p>Please specify your intended delivery zone and delivery time, and maybe modify your shipping address as well.</p>
+
+<table border="0" cellpadding="4" cellspacing="0" id="mainInfo">
+<tbody>
+<tr>
+	<td>Delivery Zone:</td>
+	<td>
+		<input type="hidden" name="trx_id" id="trx_id" value="<?php echo $trx_id; ?>" size="50" class="form" /><br /><br />
+		<?php echo form_dropdown('buyerdeliveryzone',$this->config->item('zones'),null,'id="buyerdeliveryzone"')?><br /><br />
+	</td>
+</tr>
+<tr>
+	<td>Delivery Date & Time:</td>
+	<td>
+		<input type="text" name="buyerdeliverytime" id="buyerdeliverytime" value="" size="50" class="form" /><br /><br />
+	</td>
+</tr>
+<tr>
+	<td>Delivery Address:</td>
+	<td>
+		<textarea name="shipping_address" id="shipping_address" cols="60" rows="10"></textarea>	
+	</td>
+</tr>
+<tr>
+	<td>Buyer Name:</td>
+	<td>
+		<input type="text" name="buyer_name" id="buyer_name" value="" size="50" class="form" /><br /><br />
+	</td>
+</tr>
+<tr>
+	<td>Email:</td>
+	<td>
+		<input type="text" name="email" id="email" value="" size="50" class="form" /><br /><br />
+	</td>
+</tr>
+<tr>
+	<td>Recipient Name:</td>
+	<td>
+		<input type="text" name="recipient_name" id="recipient_name" value="" size="50" class="form" /><br /><br />
+	</td>
+</tr>
+
+<tr>
+	<td>Contact Number:</td>
+	<td>
+		<input type="text" name="phone" id="phone" value="" size="50" class="form" /><br /><br />
+	</td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td>
+		<input type="button" value="Confirm Order" name="confirm" onClick="javascript:ajaxpost('confirm');" />
+		<input type="button" value="Cancel Order" name="cancel" onClick="javascript:ajaxpost('cancel');" />
+	</td>
+</tr>
+</tbody>
+</table>
+
+
+<?php print form_close()?>
+
+
+</div>
+<br /><br />
+
+<div id="deliverytable">
+</div>
+
+
+<script>
+</script>
